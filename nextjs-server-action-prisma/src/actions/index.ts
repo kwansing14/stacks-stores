c40a83d6ad888b534d008e14ff3e34f7c36ac2cf
kwansing14 <kwansing@hotmail.sg>
2023-08-12 00:53:57 +0800

    update: server action error

--- nextjs-server-action-prisma/src/actions/index.ts
+++ nextjs-server-action-prisma/src/actions/index.ts
@@ -4,14 +4,17 @@ import prisma from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 
 const addPost = async (formData: FormData, path: string) => {
-  // e.preventDefault();
   const content = formData.get('content');
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  await prisma.post.create({
-    data: {
-      text: content as string,
-    },
-  });
+  await new Promise((r) => setTimeout(r, 1000));
+  try {
+    await prisma.post.create({
+      data: {
+        text: content as string,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
   revalidatePath(path);
 };
 
