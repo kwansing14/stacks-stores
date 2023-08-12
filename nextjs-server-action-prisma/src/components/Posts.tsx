bb9e2d6f463cca7dfaf7f29df0e4d196e862510a
kwansing14 <kwansing@hotmail.sg>
2023-08-12 16:22:24 +0800

    fix: post types and build scripts

--- nextjs-server-action-prisma/src/components/Posts.tsx
+++ nextjs-server-action-prisma/src/components/Posts.tsx
@@ -1,5 +1,12 @@
 import { deletePost } from '@/actions';
+import { useTransition } from 'react';
 
+interface Post {
+  item: {
+    id: number;
+    text: string;
+  };
+}
 interface Props {
   posts: {
     id: number;
@@ -7,19 +14,28 @@ interface Props {
   }[];
 }
 
+const Post: React.FC<Post> = ({ item }) => {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <ul key={item.id} className='list-disc flex gap-2'>
+      <li className={isPending ? 'text-neutral-700' : 'text-neutral-50'}>
+        {item.text}
+      </li>
+      <button
+        className='text-red-500 text-sm'
+        onClick={() => startTransition(() => deletePost(item.id, '/'))}
+      >
+        delete
+      </button>
+    </ul>
+  );
+};
+
 const Posts: React.FC<Props> = ({ posts }) => {
   return (
     <section className='mt-4'>
       {posts.map((item) => (
-        <ul key={item.id} className='list-disc flex gap-2'>
-          <li>{item.text}</li>
-          <button
-            className='text-red-500 text-sm'
-            onClick={() => deletePost(item.id, '/')}
-          >
-            delete
-          </button>
-        </ul>
+        <Post key={item.id} item={item} />
       ))}
     </section>
   );
