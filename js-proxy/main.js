7c6b9059bf9b07ad66d304886b6185dc1dab83c3
kwansing14 <kwansing@hotmail.sg>
2023-11-05 22:11:45 +0800

    feat: done

---
+++ js-proxy/main.js
@@ -0,0 +1,38 @@
+const legacyFontSizes = {
+  extraLarge: {
+    replacementName: 'gigantic',
+    replacementValue: 'gigantic',
+  },
+  extraSmall: {
+    replacementName: 'tiny',
+    replacementValue: 'tiny',
+  },
+};
+
+const fontSizes = {
+  extraLarge: 'gigantic', // deprecated
+  extraSmall: 'tiny', // deprecated
+  gigantic: 'gigantic',
+  large: 'large',
+  medium: 'medium',
+  small: 'small',
+  tiny: 'tiny',
+};
+
+const proxyOptions = {
+  get: (target, propName, receiver) => {
+    if (propName in legacyFontSizes) {
+      console.warn(
+        `${propName} is depracted`,
+        `Use ${legacyFontSizes[propName].replacementName} instead`
+      );
+      return legacyFontSizes[propName].replacementValue;
+    }
+
+    return Reflect.get(target, propName, receiver);
+  },
+};
+
+const proxiedFontSizes = new Proxy(fontSizes, proxyOptions);
+
+export default proxiedFontSizes;
