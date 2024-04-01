edab379b8e76f1dd0fcb6f1189ff377be205ebe5
kwansing14 <kwansing@hotmail.sg>
2024-04-01 17:58:26 +0800

    feat: done

--- nextjs-google-sheets-cms/src/app/page.tsx
+++ nextjs-google-sheets-cms/src/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 
-export default function Home() {
-  console.log("test");
-  return <div>Test</div>;
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/getData");
+  const data = await res.json();
+  return (
+    <div className="flex gap-4">
+      {data.data.map((x) => (
+        <div className="p-4 y-4 flex flex-col border">
+          <div>{x.Header}</div>
+          <div>{x.Data}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
