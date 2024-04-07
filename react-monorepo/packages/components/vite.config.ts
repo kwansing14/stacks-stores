1f485bd79b5ce92f59d24e18dc14b4103baa8e6a
kwansing14 <kwansing@hotmail.sg>
2024-04-07 13:07:12 +0800

    whats this

--- react-monorepo/packages/components/vite.config.ts
+++ react-monorepo/packages/components/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "node:path";
+
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import * as packageJson from "./package.json";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// default config
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ["src/components/"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "components/index.ts"),
+      name: "Components",
+      formats: ["es", "umd"],
+      fileName: (format) => `components.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}));
