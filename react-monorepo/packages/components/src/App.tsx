72f5b5ab097bc8724109a3c93044afbec82682cf
kwansing14 <kwansing@hotmail.sg>
2024-04-12 17:05:09 +0800

    fix: header components can build but cant import

--- react-monorepo/packages/components/src/App.tsx
+++ react-monorepo/packages/components/src/App.tsx
@@ -2,12 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { isBlank } from "common";
-import { Header } from "components";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("test", Header);
 
   return (
     <>
@@ -23,13 +20,11 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-          {JSON.stringify(isBlank(count))}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      {/* <Header /> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
