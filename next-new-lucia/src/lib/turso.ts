01c9be5f99eb651593b425e16741d7c10f0b7e48
kwansing14 <kwansing@hotmail.sg>
2024-11-14 15:33:11 +0800

    template cleanup

--- next-new-lucia/src/lib/turso.ts
+++ next-new-lucia/src/lib/turso.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@libsql/client';
 
-// import '../../envConfig.ts';
-
 export const turso = createClient({
   url: process.env.DATABASE_URL,
   authToken: process.env.DATABASE_AUTH_TOKEN!,
-  // url: DATABASE_URL,
-  // authToken: DATABASE_AUTH_TOKEN,
 });
